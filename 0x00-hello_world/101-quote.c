118347f155a5fbcfb6cb4ddaed8011222b75404c
--replace-all <emmaihea6@gmail.com>
2022-12-09 03:05:26 -0800

    Modified 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  *  * main - Prints out the last part of a quote in the standard error
@@ -6,6 +5,7 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, str, 59);
 	return (1);
 }
