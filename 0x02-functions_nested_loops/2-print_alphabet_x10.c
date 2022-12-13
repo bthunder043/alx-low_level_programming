78a42acebcee88373b69c592c595985c5c286a70
--replace-all <emmaihea6@gmail.com>
2022-12-13 04:25:02 -0800

    Modified solution to task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
- * main - Prints 10 times the alphabet
+ * print_alphabet_x10 - Prints the alphabet 10 times.
  *
- *print_alphabet_x10 - prints the alphabet 10 times
- *
- * Return - void
+ * Return -Always 0
  */
 void print_alphabet_x10(void)
 {
