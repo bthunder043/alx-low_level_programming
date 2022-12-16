83eec43ba5617b2406471212f6e0c8be660c6ada
--replace-all <emmaihea6@gmail.com>
2022-12-15 20:09:07 -0800

    Solution to task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - a function that multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+
+	return (product);
+}
