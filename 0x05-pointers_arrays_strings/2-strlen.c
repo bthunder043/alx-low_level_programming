237f6bf05e402f3518eb1354b650778b85490bae
--replace-all <emmaihea6@gmail.com>
2022-12-20 02:10:16 -0800

    Betty fix

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
  * _strlen - a function that returns the length of a string
  * @s: string
  * Return: always 0
  */
+
 int _strlen(char *s)
 {
 	int count = 0;
 
-	while (*s(s + count) != '\0')
+	while (*(s + count) != '\0')
 		count++;
 	return (count);
 }
