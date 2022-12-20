51488be85876398f8e7b341417dbe1ebb0a3fd0b
--replace-all <emmaihea6@gmail.com>
2022-12-20 03:29:41 -0800

    Solution to task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints a string in reverse
+ * @s: the string
+ * Return: no return
+ */
+
+void print_rev(char *s)
+{
+	int count = o;
+
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
+
+	for (count--; count >= 0; count--)
+		_putchar(s[count]);
+	_putchar('\n');
+}
