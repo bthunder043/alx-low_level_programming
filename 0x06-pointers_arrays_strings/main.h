27f932c816f956dc57074ab17708b15955e152e4
--replace-all <emmaihea6@gmail.com>
2022-12-22 06:24:24 -0800

    bug fix

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,8 @@
 #ifndef _MAIN_H_
-#define _MAIN_H_
+#define _MAIN_H__
 
 char *_strcat(char *dest, char *src);
-char *strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
