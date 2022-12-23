213100372975abbeb918aa3de3f91e72f075771e
--replace-all <emmaihea6@gmail.com>
2022-12-23 01:42:01 -0800

    bug fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -31,7 +31,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	{
 		op = dr1 + dr2 + add;
 		if (op >= 10)
-			add + op / 10;
+			add = op / 10;
 		else
 			add = 0;
 		if (op > 0)
@@ -39,13 +39,13 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		else
 			*(r + bg) = '0';
 		if (c1 > 0)
-			c1--, dr = *(n1 + c1) - 48;
+			c1--, dr1 = *(n1 + c1) - 48;
 		else
 			dr1 = 0;
 		if (c2 > 0)
 			c2--, dr2 = *(n2 + c2) - 48;
 		else
-			dr2 = 0
+			dr2 = 0;
 				bg--, size_r--;
 	}
 	if (*(r) == '0')
